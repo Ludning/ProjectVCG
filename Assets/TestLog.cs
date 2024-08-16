8003782f667ec1616d79520b9b12d0337758f4ef
박문수 <b1372312369@gmail.com>
2024-08-16 11:47:50 +0900

    [Update] 프리팹 업데이트

---
+++ Assets/TestLog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLog : MonoBehaviour
+{
+    public void OnClickInteractableButton()
+    {
+        Debug.Log("OnClickInteractableButton");
+    }
+}
