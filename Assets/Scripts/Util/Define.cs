e9dbe323c890652002a18cb42b7d4bcae7c1d143
김현태 <rarncron@gmail.com>
2024-08-23 17:43:28 +0900

    [Update]플레이어 로직 관련 스크립트 일부수정 및 테스트

--- Assets/Scripts/Util/Define.cs
+++ Assets/Scripts/Util/Define.cs
@@ -12,6 +12,7 @@ public enum TileType
     Kitchen,
     Customer,
     Blocking,
+    Cook,
     Pass,
 }
 
--- Assets/_KHTAsset/_KHTScripts/ItemTile.cs
+++ Assets/_KHTAsset/_KHTScripts/ItemTile.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ItemTile : TileBase
 {
     [SerializeField] GameObject ItemPrefab;
+
+    //private void Update()
+    //{
+    //    if(Item == null)
+    //        OnItemSpwan();
+    //}
     public override void OnItemSpwan()
     {
         GameObject.Instantiate(ItemPrefab);
