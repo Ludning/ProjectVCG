d5169bc94019e5309f55f509051db163d8a4f74f
박문수 <b1372312369@gmail.com>
2024-08-26 17:59:52 +0900

    [Update] 기능병합중

--- Assets/Scripts/Unit/PlayerController.cs
+++ Assets/Scripts/Unit/PlayerController.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform LogicHint;
+    
     public Direction PlayerForwardType;
     public Vector2Int PlayerPosition;
     public Vector2Int PlayerForwardPosition => PlayerPosition + PlayerForward;
-/*    private PlayerTopMsg playerTopMsg;
-*/    public Vector2Int PlayerForward
+    public Vector2Int PlayerForward
     {
         get
         {
